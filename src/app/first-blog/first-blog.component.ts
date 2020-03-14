494f34429a9b0b0db66cb352f3bc2deea7410fde
cw <userchenwei@gmail.com>
2020-03-14 07:47:24 -0700

    add pagination to angular

--- src/app/first-blog/first-blog.component.ts
+++ src/app/first-blog/first-blog.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Master, Post} from '../post';
-import {MatTable} from "@angular/material/table";
+import {MatTable} from '@angular/material/table';
 import {BlogsService} from '../blogs.service';
-import {of} from "rxjs";
-import {delay} from "rxjs/operators";
+import {of} from 'rxjs';
+import {delay} from 'rxjs/operators';
 
 interface TableTypes {
   value: string;
@@ -32,15 +32,36 @@ export class FirstBlogComponent implements OnInit {
 
   ELEMENT_DATA_1: Master[] = [
     {
+      id: 1,
+      a_iden_string: 'a_iden_string_1', b_iden_string: 'b_iden_string_1',
+      dca: 100, cca: 20, name: 'name 1', cc: '111', ctr: '1111111111111111111111111111',
+      my: 'my1',
+      dt: 'dt1',
+      reg_place: 'reg_place1',
+      colo: 'colo1'
+    },
+    {
+      id: 2,
+      a_iden_string: 'a_iden_string_1', b_iden_string: 'b_iden_string_1',
+      dca: 100, cca: 20, name: 'name 1', cc: '111', ctr: '1111111111111111111111111111',
+      my: 'my1',
+      dt: 'dt1',
+      reg_place: 'reg_place1',
+      colo: 'colo1'
+    },
+    {
+      id: 3,
+      a_iden_string: 'a_iden_string_1', b_iden_string: 'b_iden_string_1',
+      dca: 100, cca: 20, name: 'name 1', cc: '111', ctr: '1111111111111111111111111111',
+      my: 'my1',
+      dt: 'dt1',
+      reg_place: 'reg_place1',
+      colo: 'colo1'
+    },
+    {
       id: 4,
-      a_iden_string: 'a_iden_string_1',
-      b_iden_string: 'b_iden_string_1',
-      // tslint:disable-next-line:max-line-length
-      dca: 100,
-      cca: 20,
-      name: 'name 1',
-      cc: '1111111111111111111111111111',
-      ctr: '1111111111111111111111111111',
+      a_iden_string: 'a_iden_string_1', b_iden_string: 'b_iden_string_1',
+      dca: 100, cca: 20, name: 'name 1', cc: '111', ctr: '1111111111111111111111111111',
       my: 'my1',
       dt: 'dt1',
       reg_place: 'reg_place1',
@@ -74,6 +95,62 @@ export class FirstBlogComponent implements OnInit {
       reg_place: 'reg_place1',
       colo: 'colo1'
     },
+    {
+      id: 7,
+      a_iden_string: 'a_iden_string_1',
+      b_iden_string: 'b_iden_string_1',
+      dca: 100,
+      cca: 20,
+      name: 'name 1',
+      cc: '1111111111111111111111111111',
+      ctr: '1111111111111111111111111111',
+      my: 'my1',
+      dt: 'dt1',
+      reg_place: 'reg_place1',
+      colo: 'colo1'
+    },
+    {
+      id: 8,
+      a_iden_string: 'a_iden_string_1',
+      b_iden_string: 'b_iden_string_1',
+      dca: 100,
+      cca: 20,
+      name: 'name 1',
+      cc: '1111111111111111111111111111',
+      ctr: '1111111111111111111111111111',
+      my: 'my1',
+      dt: 'dt1',
+      reg_place: 'reg_place1',
+      colo: 'colo1'
+    },
+    {
+      id: 9,
+      a_iden_string: 'a_iden_string_1',
+      b_iden_string: 'b_iden_string_1',
+      dca: 100,
+      cca: 20,
+      name: 'name 1',
+      cc: '1111111111111111111111111111',
+      ctr: '1111111111111111111111111111',
+      my: 'my1',
+      dt: 'dt1',
+      reg_place: 'reg_place1',
+      colo: 'colo1'
+    },
+    {
+      id: 10,
+      a_iden_string: 'a_iden_string_1',
+      b_iden_string: 'b_iden_string_1',
+      dca: 100,
+      cca: 20,
+      name: 'name 1',
+      cc: '1111111111111111111111111111',
+      ctr: '1111111111111111111111111111',
+      my: 'my1',
+      dt: 'dt1',
+      reg_place: 'reg_place1',
+      colo: 'colo1'
+    },
 
   ];
 
@@ -90,13 +167,13 @@ export class FirstBlogComponent implements OnInit {
   * */
   // tslint:disable-next-line:max-line-length
   displayedColumns: string[] = ['id',
-    'name', 'suspect',
-    'contact', 'contact_num_reg_place', 'durationtime_count_all',
-    'cnt_count_all', 'contact_location', 'user_name', 'certification_code', 'mealtype', 'capture_time_read', 'dt'];
+    'name', 'a_iden_string',
+    'b_iden_string', 'reg_place', 'dca',
+    'cca', 'cc', 'my', 'colo', 'dt', 'operation'];
 
 
   selectedRowId: string;
-  //dataSource = this.ELEMENT_DATA_1;
+  // dataSource = this.ELEMENT_DATA_1;
 
   tableTypes = ['suspect', 'undecided', 'innocence'];
 
@@ -105,26 +182,43 @@ export class FirstBlogComponent implements OnInit {
     {value: 'undecided', viewValue: '无法确定'},
     {value: 'innocence', viewValue: '没有问题'}
   ];
-  //private rowNumber: number;
-  dataSource: any;
+  //  private rowNumber: number;
+  dataSource: Master[];
+  dataShow: Master[];
 
   isLoading = true;
   private rowNumber: number;
   private tableName: string;
   private postParam: Array<string>;
 
+  length: number;
+  pageIndex = 0;
+  pageSize = 5;
+
+
 
   ngOnInit(): void {
 
-    this.blogService.getMasterRows().subscribe(data => console.log(data));
+    // this.blogService.getMasterRows().subscribe(data => console.log(data));
 
     of(this.ELEMENT_DATA_1).pipe(delay(1000))
       .subscribe(data => {
+        this.pageIndex = 0;
+        this.length = data.length;
         this.isLoading = false;
-        this.dataSource = data
+        this.dataSource = data;
+        this.dataShow = this.dataSource.slice(0, this.pageSize);
       }, error => this.isLoading = false);
 
-    //console.log(this.ELEMENT_DATA);
+    //  console.log(this.ELEMENT_DATA);
+  }
+
+  pageEvent(event) {
+
+    this.length = this.dataSource.length / this.pageSize;
+    this.dataShow  = this.dataSource.slice(event.pageIndex * event.pageSize, (event.pageIndex + 1) * event.pageSize);
+    this.table.renderRows();
+    console.log(event);
   }
 
   changeTables(event): void {
@@ -133,21 +227,19 @@ export class FirstBlogComponent implements OnInit {
     this.rowNumber = +this.postParam[1];
     this.tableName = this.postParam[0];
 
-    console.log();
+    console.log(event);
 
-    this.blogService.postTables(this.tableName,
-      (<Master>this.dataSource[0]).id).subscribe(blogs => console.log(blogs));
+   // this.blogService.postTables(this.tableName,
+     // (this.dataSource[0] as Master).id).subscribe(blogs => console.log(blogs));
 
     if (this.rowNumber > -1) {
-      this.dataSource.splice(this.rowNumber, 1);
+      this.dataShow.splice(this.rowNumber, 1);
     }
 
-    //console.log(this.dataSource[this.rowNumber-1].id);
+    //  console.log(this.dataSource[this.rowNumber-1].id);
 
 
     this.table.renderRows();
-
-
 
 
 
